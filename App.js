063311a23d1211fbec8cf96b5ae09a92d08e1289
maneesh010501 <maneeshsharma.h20@iiits.in>
2024-01-30 21:20:17 +0530

    e2

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
-// const heading = React.createElement("h1", { id: "heading", xyz: "jhgf" }, "Hello from React");
-// console.log(heading); //object
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);//converts the heading object into a h1 tag and puts up on the DOM
-
-
-{/* <div id="parent">
-    <div id="child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h1 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h1 tag</h2>
-    </div>
-</div> */}
-
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]),
-    React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")])]);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(root);
 
 root.render(parent);
+
+// npm versions->Major.Minor.Patch
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# Parcel
+
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compression
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod builds
