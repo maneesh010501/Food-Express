9cc8abc97bdeb5f6e856cc36fa91a398b6e3c212
maneesh010501 <maneeshsharma.h20@iiits.in>
2024-02-03 23:46:39 +0530

    e4 part-1

--- App.js
+++ App.js
@@ -1,31 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Components , React elements - we can put anything in anything
-const elem = <span>HI there</span>;
+/*
+Header
+- Logo
+- Nav Items
+Body 
+- Search
+- RestaurantContainer
+  - RestaurantCard
+Footer
+- Copyright
+- Links
+- Address 
+- Contact
+*/
 
-const Title = () => (
-  <h1 className="heading">
-    {elem}
-    Namaste inside JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://dcassetcdn.com/design_img/1889677/444768/444768_10393234_1889677_85203911_image.png" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
-// const data = api.getData(); 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvbjhlksmv6lybzttb6z" alt="res-logo" />
+      <h3>Maneesh Foods</h3>
+      <h5>South Indian, North Indian, Asian</h5>
+      <h5>4.4 stars</h5>
+      <h5>30</h5>
+    </div>
+  )
+}
 
-//Component Composition --> component inside component
-const HeadingComponent = () => (
-  <div className="container">
-    {/* {data} -> even if the above api passes some malicious code, jsx will escape it */}
-    {/* jsx sanitizes the data  and then executes*/}
-    {Title()}
-    {/* <h2>{10 + 100}</h2> */}
-    <h1 className="heading">Namaste Fn Component</h1>
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
-// root.render(Title);
+root.render(<AppLayout />);
